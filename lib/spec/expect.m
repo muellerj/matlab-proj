e28f694e3de9d562b2c4238cbeb38d2d1023b205
Jonas Mueller <jonas@muellers.one>
2016-04-05 10:52:11 +0200

    Guard against empty assertions cell

--- lib/spec/expect.m
+++ lib/spec/expect.m
@@ -1,4 +1,8 @@
 function expect(condition)
+%FUNCTION EXPECT
+%
+% Part of the unit test framework. Tests, whether a given condition is true and
+% stores the outcome in GLOBAL ASSERTIONS.
 
   % Guard against stupid data types
   condition = logical(condition);
@@ -7,7 +11,7 @@ function expect(condition)
     error('Condition must be a scalar boolean value!');
   end
 
-  global ASSERTIONS;
+  global ASSERTIONS; if isempty(ASSERTIONS), ASSERTIONS = {}; end
   [stack, ~] = dbstack;
 
   assertion = struct();
